8b956d8e490443c121692958bd84beea8fbb70cd
Sean Sain <ssain@sumologic.com>
2019-10-14 11:17:37 -0700

    fix get cookie file

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+eval 'set +o history' 2>/dev/null || setopt HIST_IGNORE_SPACE 2>/dev/null
+ touch ~/.gitcookies
+ chmod 0600 ~/.gitcookies
 
-touch ~/.gitcookies
-chmod 0600 ~/.gitcookies
+ git config --global http.cookiefile ~/.gitcookies
 
-git config --global http.cookiefile ~/.gitcookies
-
-tr , \\t <<\__END__ >>~/.gitcookies
-.googlesource.com,TRUE,/,TRUE,2147483647,o,git-paul.hashicorp.com=1/z7s05EYPudQ9qoe6dMVfmAVwgZopEkZBb1a2mA5QtHE
+ tr , \\t <<\__END__ >>~/.gitcookies
+.googlesource.com,TRUE,/,TRUE,2147483647,o,git-seanrsain.gmail.com=1/EoJLXwnEs8quBB5XVxvZEl8Xaoh8rdohsljBZwupZdE
 __END__
+eval 'set -o history' 2>/dev/null || unsetopt HIST_IGNORE_SPACE 2>/dev/null
