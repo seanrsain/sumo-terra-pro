557f3887638b748b230d74128cf3d9e14a13bb2b
Sean Sain <ssain@sumologic.com>
2019-10-14 12:05:10 -0700

    update script

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -1,10 +1,8 @@
-eval 'set +o history' 2>/dev/null || setopt HIST_IGNORE_SPACE 2>/dev/null
- touch ~/.gitcookies
- chmod 0600 ~/.gitcookies
+touch ~/.gitcookies
+chmod 0600 ~/.gitcookies
 
- git config --global http.cookiefile ~/.gitcookies
+git config --global http.cookiefile ~/.gitcookies
 
- tr , \\t <<\__END__ >>~/.gitcookies
+tr , \\t <<\__END__ >>~/.gitcookies
 .googlesource.com,TRUE,/,TRUE,2147483647,o,git-seanrsain.gmail.com=1/EoJLXwnEs8quBB5XVxvZEl8Xaoh8rdohsljBZwupZdE
 __END__
-eval 'set -o history' 2>/dev/null || unsetopt HIST_IGNORE_SPACE 2>/dev/null
