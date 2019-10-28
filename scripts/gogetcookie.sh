b8f087b646ae5e4ba4092de8afa12cb6084a04a7
Sean Sain <ssain@sumologic.com>
2019-10-28 15:09:29 -0700

    update script

--- scripts/gogetcookie.sh
+++ scripts/gogetcookie.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 touch ~/.gitcookies
 chmod 0600 ~/.gitcookies
 
 git config --global http.cookiefile ~/.gitcookies
 
 tr , \\t <<\__END__ >>~/.gitcookies
-.googlesource.com,TRUE,/,TRUE,2147483647,o,git-seanrsain.gmail.com=1/EoJLXwnEs8quBB5XVxvZEl8Xaoh8rdohsljBZwupZdE
+.googlesource.com,TRUE,/,TRUE,2147483647,o,git-paul.hashicorp.com=1/z7s05EYPudQ9qoe6dMVfmAVwgZopEkZBb1a2mA5QtHE
 __END__
